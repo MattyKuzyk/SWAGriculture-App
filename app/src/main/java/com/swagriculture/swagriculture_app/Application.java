405b6b8db3852ca8485fec5a1164d1435ee41859
Matthew Kuzyk <matthew.kuzyk@gmail.com>
2015-06-28 14:35:19 -0400

    Parse Push Notifications
    They work!

---
+++ app/src/main/java/com/swagriculture/swagriculture_app/Application.java
@@ -0,0 +1,41 @@
+package com.swagriculture.swagriculture_app;
+
+/**
+ * Created by Matthew on 6/28/2015.
+ */
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
+
+public class Application extends android.app.Application {
+
+    public Application() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize the Parse SDK.
+
+        Parse.initialize(this, "eJMnNRD684vVSgW6aTjuwt6aOegfEf1C7xMuvDln", "QRBj9Kk21mYxG9czBV7K7uF7zfpCcucXSZKRlbOo");
+
+        Parse.setLogLevel(Parse.LOG_LEVEL_VERBOSE);
+        // Specify an Activity to handle all pushes by default.
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
+    }
+}
